c48a58081824641957e458ecfee7bc91c83d1447
Harry Chopra <harrychopra@gmail.com>
2020-02-15 13:34:31 +0000

    Recursion

--- arrays.js
+++ arrays.js
@@ -18,3 +18,37 @@ multiplyAll([
 	[3, 4],
 	[5, 6, 7]
 ]);
+
+//Recursive operations
+// array input
+
+const arraySum = arr => {
+	while (arr.length > 1) {
+		return arr.pop() + arraySum(arr);
+	}
+	return arr[0];
+};
+
+// from nums 1-10
+const numSum = num => {
+	while (num > 1) {
+		return num + numSum(num - 1);
+	}
+	return 1;
+};
+
+// index 0 - 9 or 10th position
+const numSum = pos => {
+	while (pos > 1) {
+		return --pos + numSum(pos);
+	}
+	return 0;
+};
+
+//Fibonacci
+const fib = num => {
+	while (num > 1) {
+		return fib(num - 1) + fib(num - 2);
+	}
+	return num;
+};
